7bbb17e3737e0ec46d9fcf068367003cc47ac097
ningfu <592773569@qq.com>
2017-02-08 02:26:24 +0800

    add musicplay to new weydio project

--- weydio/app/src/main/java/com/ninghoo/beta/weydio/MainActivity.java
+++ weydio/app/src/main/java/com/ninghoo/beta/weydio/MainActivity.java
@@ -3,12 +3,23 @@ package com.ninghoo.beta.weydio;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.ninghoo.beta.weydio.Adapter.MusicListAdapter;
+import com.ninghoo.beta.weydio.model.Audio;
+import com.ninghoo.beta.weydio.model.MediaDetails;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
     private RecyclerView mRecyMusiclist;
+
+    private MusicListAdapter adapter;
+
+    private ArrayList<Audio> la;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,7 +35,13 @@ public class MainActivity extends AppCompatActivity
     private void initRecyMusicList()
     {
         mRecyMusiclist = (RecyclerView) findViewById(R.id.recycl_musiclist);
+        mRecyMusiclist.setLayoutManager(new LinearLayoutManager(this));
 
+        // 由于getAudioList是static方法，所以可以直接通过类名调用。
+        la =  MediaDetails.getAudioList(MainActivity.this);
+
+        adapter = new MusicListAdapter(this, la);
+        mRecyMusiclist.setAdapter(adapter);
     }
 
     private void hideActionBar()
